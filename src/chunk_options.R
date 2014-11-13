46c68f8036991861b5b33bbb02cf408f644cc067
Denis Haine <denis.haine@gmail.com>
2014-11-13 12:50:47 -0500

    Adding files for website

---
+++ src/chunk_options.R
@@ -0,0 +1,8 @@
+# These settings control the behavior of all R chunks.
+# For more information on available chunk options, see
+# http://yihui.name/knitr/options#chunk_options
+
+library("knitr")
+opts_chunk$set(tidy = FALSE, results = "markup", comment = NA,
+               fig.align = "center")
+
