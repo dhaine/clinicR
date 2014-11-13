e3287f3320414091753b08663e5fb5ab41cb8bae
Denis Haine <denis.haine@gmail.com>
2014-11-13 12:51:04 -0500

    Adding makefiles

---
+++ src/rmd.mk
@@ -0,0 +1,36 @@
+#======================================================================
+# Create Markdown versions of R Markdown files.
+#
+# 'make -f rmd.mk'
+# ======================================================================
+
+#----------------------------------------------------------------------
+# Specify the default target before any other targets are defined so
+# that we're sure which one Make will choose.
+#----------------------------------------------------------------------
+
+all : rmd
+
+#----------------------------------------------------------------------
+# Rules.
+#----------------------------------------------------------------------
+
+# Chunk options for knitr
+CHUNK_OPTS = chunk_options.R
+
+# R Markdown files.  Add patterns here to convert files stored in
+# other locations.
+RMD_SRC = \
+	$(wildcard ??????-*.Rmd)
+
+# Files converted to Markdown.
+RMD_TX = $(patsubst %.Rmd,%.md,$(RMD_SRC))
+
+# Convert a .Rmd to .md.
+%.md: %.Rmd $(CHUNK_OPTS)
+	cd $$(dirname $<) && \
+        Rscript -e "knitr::knit('$$(basename $<)', \
+                                output = '$$(basename $@)')"
+
+# Target.
+rmd : $(RMD_TX)
